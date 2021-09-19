a69a4f0497849d9c20f6b77870ca77ae92f5c7bb
Zhigen Zhao <zhaozhigen@gmail.com>
2021-09-19 18:07:23 -0400

    updated plotting script

--- Kuka_examples/plot_monitor.py
+++ Kuka_examples/plot_monitor.py
@@ -4,7 +4,7 @@ import csv
 
 
 def main():
-    filename = "/home/zhigen/code/stable-baselines3/Kuka_examples/saved_models/monitor.csv"
+    filename = "/home/zhigen/code/stable-baselines3/Kuka_examples/saved_models/monitor (copy).csv"
     with open(filename) as csvfile:
         training_monitor = csv.reader(csvfile)
         
@@ -12,8 +12,40 @@ def main():
         for (i, row) in enumerate(training_monitor):
             if i > 1:
                 reward.append(float(row[0]))
-    
-        plt.plot(np.arange(len(reward)), np.log10(-np.array(reward)))
+        reward = np.array(reward)
+
+        reward_vec = []
+        reward_std = []
+        i = 0
+        n = 5000
+        while i < len(reward):
+            if i+n < len(reward):
+                temp = reward[i:i+n]
+            else:
+                temp = reward[i:]
+            
+            m = np.mean(temp)
+            s = np.std(temp)
+
+            reward_vec.append(m)
+            reward_std.append(s)
+
+            i += n
+
+
+        plt.plot(np.arange(len(reward_vec))*n, -np.array(reward_vec), linewidth=3)
+        plt.plot(np.arange(len(reward_vec))*n, np.ones_like(reward_vec)*1750, linestyle="dashed", linewidth=3)
+        plt.fill_between(np.arange(len(reward_vec))*n, 
+            -np.array(reward_vec)+np.array(reward_std),
+            -np.array(reward_vec)-np.array(reward_std),
+            alpha=0.5)
+        plt.yscale("log")
+        plt.xlabel("Timestep", fontsize=24)
+        plt.xticks(fontsize=14)
+        plt.ylabel("Cost", fontsize=24)
+        plt.yticks(fontsize=16)
+        plt.legend(["PPO", "VERONICA baseline"], fontsize=16)
+        plt.xlim([0, 300000])
         plt.show()
 
 if __name__=="__main__":
